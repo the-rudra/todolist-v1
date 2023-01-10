23d97a51131616137738598a958f7f4a8755dd20
rudra <itsrudrapratapdev@gmail.com>
2023-01-10 16:22:34 +0530

    add a work list

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -18,13 +19,28 @@ app.get("/", (req, res) => {
     }
     let currentDay = today.toLocaleDateString("en-US", options);
 
-    res.render('list', {kindOfDay: currentDay, newListItem: items});
+    res.render('list', {listTitle: currentDay, newListItem: items});
+});
+
+app.get("/work", (req, res) => {
+    res.render('list', {listTitle: "Work List", newListItem: workItems});
+});
+
+app.get("/about", (req, res) => {
+    res.render('about');
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if(req.body.button === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+    
 });
 
 app.listen(3000, function () {
